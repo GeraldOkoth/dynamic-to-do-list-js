ec8e5cff7bc9960e771c4bd9fda031979e458a30
Gerald Okoth <okothgerald449@gmail.com>
2026-09-11 23:18:22 +0300

    refactor: refine UI

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const addButton = document.getElementById("add-task-btn");
   const taskInput = document.getElementById("task-input");
-  const taskList = document.getElementsByClassName("task-list")[0];
+  const taskList = document.querySelector(".task-list");
 
   let taskArray = [];
 
@@ -19,6 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
   function addTask(taskText = "", save = true) {
     if (!taskText) {
       taskText = taskInput.value.trim();
+      taskText = taskText.charAt(0).toUpperCase() + taskText.slice(1); // Capitalize the first letter
       if (taskText === "") {
         alert("Please enter a task!");
         return;
@@ -27,19 +28,30 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const listItem = document.createElement("li");
     const span = document.createElement('span');
-    span.classList.add('task-text');
-    listItem.textContent = taskText;
+    const span1 = document.createElement('span');
+    const span2 = document.createElement('span');
+
+    listItem.classList.add("task-item");
+    span.classList.add('flex-1');
+    span1.classList.add('checked');
+    span2.classList.add('task-text');
+    span2.textContent = taskText;
+    span.appendChild(span1);
+    span.appendChild(span2);
+    listItem.appendChild(span);
 
     const removeBtn = document.createElement("button");
     removeBtn.classList.add("remove-btn");
     removeBtn.addEventListener("click", () => {
       taskList.removeChild(listItem);
-      taskArray = taskArray.filter((task) => task !== taskText);
+      const taskIndex = taskArray.indexOf(taskText);
+      if (taskIndex !== -1) {
+        taskArray.splice(taskIndex, 1);
+      }
       localStorage.setItem("tasks", JSON.stringify(taskArray));
     });
 
     listItem.appendChild(removeBtn);
-    listItem.appendChild(span);
     taskList.appendChild(listItem);
 
     if (save) {
